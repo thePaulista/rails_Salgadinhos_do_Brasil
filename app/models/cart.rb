f7b3e45ed02e21909e6d627ce3aaabfe1729c26b
Hedy <hedywoo@gmail.com>
2016-05-02 23:21:06 -0600

    Add CartsController

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,20 @@
 class Cart
   attr_reader :items
 
-  def initialize
-    @items = []
+  def self.build_from_hash hash
+    items = if hash["cart"] then
+      hash["cart"]["items"].map do |item_data|
+        CartItem.new item_data["product_id"], item_data["quantity"]
+      end
+    else
+      []
+    end
+
+    new items
+  end
+
+  def initialize items =[]
+    @items = items
   end
 
  def add_item product_id
@@ -16,5 +28,21 @@ class Cart
 
   def empty?
     @items.empty?
+  end
+
+  def serialize
+    #this block is executing something
+    items = @items.map  do  |item|
+      {
+        "product_id" => item.product_id,
+        "quantity" => item.quantity
+      }
+    end
+
+#and the hash that is being returned
+    {
+      "items" => items
+    }
+    }
   end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class CartTest < MiniTest::Test
   def test_adds_one_item
@@ -24,4 +24,29 @@ class CartTest < MiniTest::Test
 
     assert_kind_of Product, cart.items.first.product
   end
+
+  def test_serializes_to_hash
+    cart = Cart.new
+    cart.add_item 1
+
+    assert_equal cart.serialize, session_hash["cart"]
+  end
+
+  def test_builds_from_hash
+    cart = Cart.build_from_hash session_hash
+    assert_equal 1, cart.items.first.product_id
+  end
+
+  private
+
+  def session_hash
+    {
+      "cart" => {
+        "items" => [
+          { "product_id" => 1, "quantity" => 1 }
+        ]
+      }
+    }
+  end
 end
+
